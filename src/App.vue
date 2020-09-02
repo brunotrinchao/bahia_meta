31a114939f43cce75c7f612bc2cf3bdf9fe286fa
Bruno Trinchao <brunotrinchao@gmail.com>
2020-09-02 08:31:15 -0300

    Primeiro versionamento

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container-fluid">
+    <div class="py-5 text-center">
+      <img
+        class="d-block mx-auto mb-4"
+        src="https://upload.wikimedia.org/wikipedia/pt/9/90/ECBahia.png"
+        alt=""
+        height="100"
+      />
+      <h2>Pontuação por bloco do campeonato brasileiro 2020</h2>
+      <p class="lead"></p>
+    </div>
+    <Home />
+    <div class="pt-4 my-md-5 pt-md-5 border-top"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from './components/home';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
